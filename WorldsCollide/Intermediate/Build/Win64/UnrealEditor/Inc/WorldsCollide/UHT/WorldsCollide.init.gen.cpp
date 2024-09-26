53889a487a1b0bc081ac57338042f9b62239a14c
Lev <63475577+LevWorking@users.noreply.github.com>
2024-09-26 16:21:58 -0400

    Fog of War Stuff

---
+++ WorldsCollide/Intermediate/Build/Win64/UnrealEditor/Inc/WorldsCollide/UHT/WorldsCollide.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeWorldsCollide_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_WorldsCollide;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_WorldsCollide()
+	{
+		if (!Z_Registration_Info_UPackage__Script_WorldsCollide.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/WorldsCollide",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x3EA84A0F,
+				0x6D63E345,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_WorldsCollide.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_WorldsCollide.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_WorldsCollide(Z_Construct_UPackage__Script_WorldsCollide, TEXT("/Script/WorldsCollide"), Z_Registration_Info_UPackage__Script_WorldsCollide, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x3EA84A0F, 0x6D63E345));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
